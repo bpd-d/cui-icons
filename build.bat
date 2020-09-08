eb0a57e3b2468f84c6498e5f360d4f8a5417a304
bartoszp32 <bartoszp32@gmail.com>
2020-09-08 16:50:29 +0200

    Update to 0.1.0

---
+++ build.bat
@@ -0,0 +1,1 @@
+python C:\Users\barto\Documents\Development\bpd_dev\svg-cleaner\main.py -i "C:\Users\barto\Documents\Development\bpd_dev\cui-icons\raw" -o "C:\Users\barto\Documents\Development\bpd_dev\cui-icons\dist" --clean --js
